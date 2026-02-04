ade9ecc63ef04c289238c8fd4d77ec3dfe59b435
Saichithra Puppala <saichithra.puppala@aaseya.com>
2026-02-04 17:05:14 +0530

    changes done for the React setup and EPIC-IICP 16

---
+++ src/components/UploadDocuments.jsx
@@ -0,0 +1,15 @@
+const UploadDocuments = () => {
+  return (
+    <Box p={4}>
+      <Typography fontSize={20} fontWeight={700}>
+        Upload Documents
+      </Typography>
+
+      <Typography mt={2} color="text.secondary">
+        Dummy document data will be added here later.
+      </Typography>
+    </Box>
+  );
+};
+
+export default UploadDocuments;
